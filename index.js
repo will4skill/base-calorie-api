1ccf41b2ab519cb30f816059fe1d73dfe07a2a76
Tim Williams <jtimwill@gmail.com>
2020-02-20 10:11:46 -0500

    Refactored code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // 1. https://github.com/vpulim/node-soap#soaplistenserver-path-services-wsdl-callback---create-a-new-soap-server-that-listens-on-path-and-provides-services
 // 2. https://gitter.im/vpulim/node-soap/archives/2017/09/04
 // 3. https://stackoverflow.com/questions/8655252/node-js-how-to-consume-soap-xml-web-service
+// 4. https://codewithmosh.com/p/the-complete-node-js-course
 const soap = require('soap');
 const express = require('express');
 const cors = require('cors');
@@ -12,9 +13,9 @@ var myService = {
   BaseCalorie_Service: {
     BaseCalorie_Port: {
       getCalories: function(args) {
-        console.log('getCalories called');
-        console.log(args.weight_lbs['$value']);
-        let bmr = 10*Number(args.weight_lbs['$value'])/2.205 + 6.25*2.54*Number(args.height_in['$value']) - 5*Number(args.age_yrs['$value']);
+        let bmr = 10*Number(args.weight_lbs['$value'])/2.205 +
+          6.25*2.54*Number(args.height_in['$value']) -
+          5*Number(args.age_yrs['$value']);
         if (args.sex === "f") {
           bmr -= 161;
         } else {
